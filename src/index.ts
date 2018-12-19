82ae6b81821330f7b79729f41e8eec0e6f2a3cb4
Oscar Nylander <oscar@hedvig.com>
2018-12-19 16:59:51 +0100

    Implement a sort-of workable cache mechanism

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,46 @@
-// this is your lib entry
+import { ApolloLink, ExecutionResult, Observable } from 'apollo-link'
+import { Redis } from 'ioredis'
 
-import { not } from 'rambda'
+export const createCacheLink = (redis: Redis) =>
+  new ApolloLink((operation, forward) => {
+    const query = JSON.stringify(operation.query)
+    return new Observable<ExecutionResult>((observer) => {
+      redis.get(query).then((res) => {
+        if (res) {
+          observer.next(JSON.parse(res) as ExecutionResult)
+          observer.complete()
+        } else if (forward) {
+          forward(operation).forEach((linkRes) => {
+            updateCacheIfErrorFreeAndOutdated(redis, query, linkRes, res)
+            observer.next(linkRes)
+            observer.complete()
+          })
+          return
+        }
+        if (forward) {
+          setTimeout(() => {
+            forward(operation).forEach((linkRes) => {
+              updateCacheIfErrorFreeAndOutdated(redis, query, linkRes, res)
+            })
+          }, 0)
+        }
+      })
+    })
+  })
 
-export const hej = not(2)
+const updateCacheIfErrorFreeAndOutdated = (
+  redis: Redis,
+  query: string,
+  linkRes: ExecutionResult,
+  cachedRes: string | null,
+) => {
+  if (linkRes.errors) {
+    return
+  }
+
+  const linkResAsString = JSON.stringify(linkRes)
+  if (linkResAsString === cachedRes) {
+    return
+  }
+  redis.set(query, linkResAsString)
+}
